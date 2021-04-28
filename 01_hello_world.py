e0d09ee11aeff724ee823f3b9795eb4b4820246e
FModabber <76422648+FModabber@users.noreply.github.com>
2021-04-28 09:03:20 -0700

    first file

---
+++ 01_hello_world.py
@@ -0,0 +1,8 @@
+print("hello, world")
+# This line is a comment.
+
+print("Welcome to Ada!")
+print("we're so glad you're here.")
+
+# use \n to seprate two lines.
+print("Welcome to Ada!\nwe're so glad you're here.")
