b460c22e697ddf5a58bb610b506f44f519d62be0
FModabber <76422648+FModabber@users.noreply.github.com>
2021-05-03 17:00:39 -0700

    l2 project

---
+++ 03_branching_project.py
@@ -0,0 +1,25 @@
+#Rock, Paper, Scissors
+player1 = "scissors"
+player2 = "scissors"
+
+if player1 == 'paper':
+    if player2 == 'rock':
+     print("Player 1 won!")
+    elif player2 == "scissors":
+        print("player 2 won!")
+    else:
+        print("It's a tie!")
+elif player1 == 'rock':
+    if player2 == 'paper':
+        print("Player 2 won!")
+    elif player2 == "scissors":
+        print("player 1 won!")
+    else:
+        print("It's a tie!")
+elif player1 == 'scissors':
+    if player2 == 'rock':
+         print("Player 2 won!")
+    elif player2 == "paper":
+        print("player 2 won!")
+    else:
+        print("It's a tie!")
