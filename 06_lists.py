1ed555548550d2c42f69d3ad8aa7c9ac03d4e85b
FModabber <76422648+FModabber@users.noreply.github.com>
2021-05-07 20:17:07 -0700

    l6

---
+++ 06_lists.py
@@ -0,0 +1,14 @@
+#empty list
+my_list = []
+
+numbers = [1, 2, 3, 4]
+print(numbers)
+
+nums = list(range(1,6))
+print(nums)
+
+my_list = list("banana")
+print(my_list)
+
+my_list[-1] = "apple"
+print(my_list)
